760f6153631f87fcae4e9dbfbf5b0a4ca89be779
CleverlyDone <pierregober@gmail.com>
2021-07-21 12:23:59 -0700

    Working version with support to nellet

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const IS_OFFLINE = process.env.NODE_ENV !== "production";
 const GRUBBER_USERS = process.env.TABLE_GRUBBER_USERS;
 const GRUBBER_RESTAURANTS = process.env.TABLE_GRUBBER_RESTAURANTS;
 const GRUBBER_DIETS = process.env.TABLE_GRUBBER_DIETS;
+const NELLET_USERS = process.env.TABLE_NELLET_USERS;
 
 const dynamoDb =
   IS_OFFLINE === true
@@ -164,6 +165,65 @@ router.post("/grubber", (req, res) => {
     res.json({
       id,
       name,
+    });
+  });
+});
+
+router.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+
+  const params = {
+    TableName: NELLET_USERS,
+    Key: {
+      id,
+    },
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      res.status(400).json({ error: "Error retrieving nellet user" });
+    }
+    if (result && result.Item) {
+      console.log(result);
+      res.json(result.Item);
+    } else {
+      res.status(404).json({ error: `Nellet user with id: ${id} not found` });
+    }
+  });
+});
+
+router.post("/nellet/user/register", (req, res) => {
+  console.log("req for nellet: ", req.body);
+  const defaultPreferences = { darkMode: false };
+  const email = req.body.email ?? false;
+  const profilePicture =
+    req.body.profilePicture ??
+    "http://www.quickmeme.com/img/4d/4d56e45853983bfeedced94719e78b2869e21252c3d85105f7b56320b8f959ab.jpg";
+  const givenName = req.body.givenName ?? false;
+  const familyName = req.body.familyName ?? false;
+  const preferences = req.body.preferences ?? defaultPreferences;
+  const id = req.body.sub ?? uuid.v4();
+  const lastModified = req.body.updated_at ?? false;
+
+  const params = {
+    TableName: NELLET_USERS,
+    Item: {
+      id,
+      email,
+      profilePicture,
+      givenName,
+      familyName,
+      preferences,
+      lastModified,
+    },
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      res.status(400).json({ error: "Could not create nellet user" });
+    }
+    res.json({
+      id,
     });
   });
 });
